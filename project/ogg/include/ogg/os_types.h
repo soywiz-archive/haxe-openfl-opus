bfa3f12d2d8293497cfbf17bd9efee6d04fb7344
soywiz <soywiz@gmail.com>
2012-10-23 23:28:23 +0200

    - Added Android binaries

--- project/ogg/include/ogg/os_types.h
+++ project/ogg/include/ogg/os_types.h
@@ -140,7 +140,13 @@
 
 #else
 
-#  include <ogg/config_types.h>
+#    include <sys/types.h>
+     typedef short ogg_int16_t;
+     typedef unsigned short ogg_uint16_t;
+     typedef int ogg_int32_t;
+     typedef unsigned int ogg_uint32_t;
+     typedef long long ogg_int64_t;
+     typedef unsigned long long ogg_uint64_t;
 
 #endif
 
